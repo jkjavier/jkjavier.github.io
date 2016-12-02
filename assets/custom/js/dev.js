fb90566d9ff1729e5289e94552964722c3cf12f6
Joseph Kim Javier <Joseph Kim Javier>
2016-12-02 16:22:31 +0800

    Skilsl

--- assets/custom/js/dev.js
+++ assets/custom/js/dev.js
@@ -6,16 +6,16 @@
     var skills = $('#skillList');
 
     $.ajax({
-        url: 'https://jkjavier.github.io/data/experience.json',
-        //url: './data/experience.json',
+        //url: 'https://jkjavier.github.io/data/experience.json',
+        url: './data/experience.json',
         dataType: 'json',
         type: 'get',
         success: function(response){
             $.each(response, function(resp){
-            	employment.append('<li><b>'+this.position+'</b> / '+this.company+
-        		'<br/><span class="workDuration"> '+ this.startDate +
-        		' - '+ this.endDate + '</span> '+
-        		' <span class="companyInfo">'+ this.address + '</span>'+
+                employment.append('<li><b>'+this.position+'</b> / '+this.company+
+                '<br/><span class="workDuration"> '+ this.startDate +
+                ' - '+ this.endDate + '</span>'+
+                '<br/><span class="companyInfo">'+ this.address + '</span>'+
                 '<br/><div class="jobDescription"><span >'+ this.description + '</span></div>'+
                 '<br/></li>');
             });
@@ -23,20 +23,36 @@
     });
 
     $.ajax({
-        url: 'https://jkjavier.github.io/data/skills.json',
-        //url: './data/skills.json',
+        //url: 'https://jkjavier.github.io/data/skills.json',
+        url: './data/skills2.json',
         dataType: 'json',
         type: 'get',
         success: function(response){
-            $.each(response, function(res){
-            	skills.append(this.softwareDevelopment);
-            });
+            console.log(response.softwareDevelopment);
+            
+            skills.append('<li class="skillsList"><span class="skillsHeader">Software Development : </span><span class="skillsContent">'+response.softwareDevelopment+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">Web Development : </span><span class="skillsContent">'+response.webDevelopment+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">Databases : </span><span class="skillsContent">'+response.databases+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">Frameworks : </span><span class="skillsContent">'+response.frameworks+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">Build Tools : </span><span class="skillsContent">'+response.buildTools+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">APIs : </span><span class="skillsContent">'+response.apis+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">Platforms : </span><span class="skillsContent">'+response.platforms+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">Version Control : </span><span class="skillsContent">'+response.versionControl+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">Unit Tests : </span><span class="skillsContent">'+response.unitTests+'</li><br/>');
+            skills.append('<li class="skillsList"><span class="skillsHeader">Others : </span><span class="skillsContent">'+response.others+'</li><br/>');
+//            $.each(response, function(res){
+//                console.log(res);
+//                console.log(res);
+//                $.each(res, function(content){
+//                    console.log(this);
+//                });
+//            });
         }
     });
 
     $.ajax({
-        url: 'https://jkjavier.github.io/data/personal.json',
-        //url: './data/personal.json',
+        //url: 'https://jkjavier.github.io/data/personal.json',
+        url: './data/personal.json',
         dataType: 'json',
         type: 'get',
         success: function(response){
@@ -45,8 +61,8 @@
     });
 
     $.ajax({
-        url: 'https://jkjavier.github.io/data/education.json',
-        //url: './data/education.json',
+        //url: 'https://jkjavier.github.io/data/education.json',
+        url: './data/education.json',
         dataType: 'json',
         type: 'get',
         success: function(response){
